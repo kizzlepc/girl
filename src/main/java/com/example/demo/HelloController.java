9d70f5c3ba09d07641ddab9b3904a590642c567e
kizzlepc <zhangkaipc@163.com>
2018-04-15 12:01:55 +0800

    从url中获取数据简洁

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,20 +2,23 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/hello")
 public class HelloController {
 
 	@Autowired
 	private GirlProperties girlProperties;
 	
-	@RequestMapping(value="/hello", method=RequestMethod.GET)
-	public String say() {
+	@RequestMapping(value="/{id1}/say/{id2}", method=RequestMethod.GET)
+	public String say(@PathVariable("id1") Integer id1, @PathVariable("id2") Integer id2) {
 		//return "Hello Spring Boot!";
 		//return cupSize+":"+age;
-		return girlProperties.getCupSize();
+		//return girlProperties.getCupSize();
+		return "id1:"+id1+" - id2:"+id2;
 	}
 }
