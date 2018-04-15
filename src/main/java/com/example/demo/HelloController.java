5b47f7582cefeec584efa7da0c6557a4f583de80
kizzlepc <zhangkaipc@163.com>
2018-04-15 12:13:34 +0800

    设定默认值

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,8 +16,9 @@ public class HelloController {
 	@Autowired
 	private GirlProperties girlProperties;
 	
-	@RequestMapping(value="/say", method=RequestMethod.GET)
-	public String say(@RequestParam("id") Integer id1) {
+	//@RequestMapping(value="/say", method=RequestMethod.GET)
+	@GetMapping(value="/say")
+	public String say(@RequestParam(value="id", required=false, defaultValue="0") Integer id1) {
 		return "id1:"+id1;
 	}
 }
