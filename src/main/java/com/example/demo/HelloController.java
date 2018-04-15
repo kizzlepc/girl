7042ec72d1f327c246dd0ce52db5d84d80dd3025
kizzlepc <zhangkaipc@163.com>
2018-04-15 12:06:46 +0800

    从url后面？的参数中获取数据

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,11 +15,8 @@ public class HelloController {
 	@Autowired
 	private GirlProperties girlProperties;
 	
-	@RequestMapping(value="/{id1}/say/{id2}", method=RequestMethod.GET)
-	public String say(@PathVariable("id1") Integer id1, @PathVariable("id2") Integer id2) {
-		//return "Hello Spring Boot!";
-		//return cupSize+":"+age;
-		//return girlProperties.getCupSize();
-		return "id1:"+id1+" - id2:"+id2;
+	@RequestMapping(value="/say", method=RequestMethod.GET)
+	public String say(@RequestParam("id") Integer id1) {
+		return "id1:"+id1;
 	}
 }
