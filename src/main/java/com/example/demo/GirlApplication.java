c4f92b1c73569406a670df4757ab60b416f0e1f0
kizzlepc <zhangkaipc@163.com>
2018-04-16 11:02:25 +0800

    Springboot基础-添加日志、配置http编码为

--- src/main/java/com/example/demo/GirlApplication.java
+++ src/main/java/com/example/demo/GirlApplication.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+//@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class GirlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GirlApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "你好， Spring boot！";
 	}
 }
